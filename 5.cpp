374cacbf4ad8e4ab98c4046c206bfae0bbd183a1
Your Name <Panchalom136@gmail.com>
2026-04-12 14:27:32 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int, int> freq;
+
+    for (int num : nums) {
+        freq[num]++;
+    }
+
+    // Max Heap: pair<frequency, element>
+    priority_queue<pair<int, int>> pq;
+
+    for (auto it : freq) {
+        pq.push({it.second, it.first});
+    }
+
+    vector<int> result;
+
+    // Get top k elements
+    while (k--) {
+        result.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,2,2,3,3,3};
+    int k = 2;
+
+    vector<int> ans = topKFrequent(nums, k);
+
+    for (int x : ans) cout << x << " ";
+}
