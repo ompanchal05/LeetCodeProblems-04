7d6e5332fcdabecc302b4dd87856f29bdbf9a4d7
Your Name <Panchalom136@gmail.com>
2026-06-21 16:16:20 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,16 @@
+# two sum in the optimal solution
+'''
+Last Executed Input
+Use Testcase
+nums =[2,7,11,15]
+target = 9
+'''
+
+class Solution:
+    def twoSum(self,nums,target):
+
+        for i in range(len(nums)):
+            for j in range(i+1 (len(nums))):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
